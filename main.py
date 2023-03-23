36e48d120e4f36019951443ea12382ddeae3af30
ajay <ajaybeniwal070@gmail.com>
2023-03-23 12:00:05 +0530

    update main

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ app = FastAPI(
 
 
 # Load the model for irrelavent images(+18)
-model_nsfw = predict.load_model('./nsfw_mobilenet2.224x224.h5')
+# model_nsfw = predict.load_model('./nsfw_mobilenet2.224x224.h5')
 
 # Load the model for image Label(Classification)
 model = load_model('keras_model.h5',compile=False)
@@ -80,176 +80,179 @@ def predict_img(image: Image.Image):
     class_name = class_names[index]
     confidence_score = prediction[0][index]
     
-    if class_name[2:] == "Not_valid\n":
+    # if class_name[2:] == "Not_valid\n":
     
-        prediction_dict= {
+    #     prediction_dict= {
+    #     "response": {
+    #         "solutions": {
+    #             "re_roomtype_eu_v2": {
+                     
+    #                  "predictions": [
+    #                                     {
+    #                                     "confidence": str(confidence_score),
+    #                                     "label" : "false"
+    #                                     }
+    #                                 ],
+    #                                 "top_prediction":{
+    #                                     "confidence": str(confidence_score),
+    #                                         "label": "false"
+    #                                 }
+    #                             }
+    #                         }
+    #                     }
+    #                 }
+        
+    #     return prediction_dict
+    # else:
+    #labels = ['Bathroom','Room-bedroom','Living_Room','Outdoor_building','Kitchen','Non_Related','Garden','Plot','Empty_room']
+    labels = ['Bathroom','Bedroom','Living Room','Exterior View','Kitchen','Non_Related','Garden','Plot','Room','Swimming Pool','Gym','Parking','Map Location','Balcony','Floor Plan']
+
+
+    data = np.ndarray(shape=(1,224, 224, 3), dtype=np.float32)
+    size = (224, 224)
+
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    image_array = np.asarray(image)
+    #image.show()
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+    img_array_final = normalized_image_array[:, :, :3]
+    #print("shape: ", img_array_final.shape)
+    data[0] = img_array_final
+    result = model.predict(data)
+    #result1 = sorted(result, reverse=True)
+    
+    arr_sorted = -np.sort(-result,axis=1)
+    top_five = arr_sorted[:,:5]
+    top_five_array = result.argsort()
+    top_five_array1 = top_five_array.tolist()
+    top1 = top_five_array1[0][-1]
+    top2 = top_five_array1[0][-2]
+    top3 = top_five_array1[0][-3]
+    top4 = top_five_array1[0][-4]
+    top5 = top_five_array1[0][-5]
+
+    index_max = np.argmax(result)
+
+
+    
+    prediction_dict = {
         "response": {
             "solutions": {
                 "re_roomtype_eu_v2": {
-                     
-                     "predictions": [
-                                        {
-                                        "confidence": str(confidence_score),
-                                        "label" : "false"
-                                        }
-                                    ],
-                                    "top_prediction":{
-                                        "confidence": str(confidence_score),
-                                            "label": "false"
-                                    }
-                                }
-                            }
+                    
+                    "predictions": [
+                        {
+                            "confidence": str(top_five[0][0]),
+                            "label": str(labels[top1])
+                        },
+                        {
+                            "confidence": str(top_five[0][1]),
+                            "label": str(labels[top2])
+                        },
+                        {
+                            "confidence": str(top_five[0][2]),
+                            "label": str(labels[top3])
+                        },
+                        {
+                            "confidence": str(top_five[0][3]),
+                            "label": str(labels[top4])
+                        },
+                        {
+                            "confidence": str(top_five[0][4]),
+                            "label": str(labels[top5])
                         }
-                    }
-        
-        return prediction_dict
-    else:
-        #labels = ['Bathroom','Room-bedroom','Living_Room','Outdoor_building','Kitchen','Non_Related','Garden','Plot','Empty_room']
-        labels = ['Bathroom','Bedroom','Living Room','Exterior View','Kitchen','Non_Related','Garden','Plot','Room','Swimming Pool','Gym','Parking','Map Location','Balcony','Floor Plan']
+                    ],
 
-
-        data = np.ndarray(shape=(1,224, 224, 3), dtype=np.float32)
-        size = (224, 224)
-
-        image = ImageOps.fit(image, size, Image.ANTIALIAS)
-        image_array = np.asarray(image)
-        #image.show()
-        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
-        img_array_final = normalized_image_array[:, :, :3]
-        #print("shape: ", img_array_final.shape)
-        data[0] = img_array_final
-        result = model.predict(data)
-        #result1 = sorted(result, reverse=True)
-        
-        arr_sorted = -np.sort(-result,axis=1)
-        top_five = arr_sorted[:,:5]
-        top_five_array = result.argsort()
-        top_five_array1 = top_five_array.tolist()
-        top1 = top_five_array1[0][-1]
-        top2 = top_five_array1[0][-2]
-        top3 = top_five_array1[0][-3]
-        top4 = top_five_array1[0][-4]
-        top5 = top_five_array1[0][-5]
-
-        index_max = np.argmax(result)
-
-
-        
-        prediction_dict = {
-            "response": {
-                "solutions": {
-                    "re_roomtype_eu_v2": {
-                        
-                        "predictions": [
-                            {
-                                "confidence": str(top_five[0][0]),
-                                "label": str(labels[top1])
-                            },
-                            {
-                                "confidence": str(top_five[0][1]),
-                                "label": str(labels[top2])
-                            },
-                            {
-                                "confidence": str(top_five[0][2]),
-                                "label": str(labels[top3])
-                            },
-                            {
-                                "confidence": str(top_five[0][3]),
-                                "label": str(labels[top4])
-                            },
-                            {
-                                "confidence": str(top_five[0][4]),
-                                "label": str(labels[top5])
-                            }
-                        ],
-
-                       
-                        "top_prediction":{
-                            "confidence": str(result[0][index_max]),
-                                "label": str(labels[index_max])
-                        }
+                    
+                    "top_prediction":{
+                        "confidence": str(result[0][index_max]),
+                            "label": str(labels[index_max])
                     }
                 }
             }
         }
+    }
 
-        if labels[top1] == 'Non_Related' and top_five[0][0]*100 > 80:
+    if labels[top1] == 'Non_Related' and top_five[0][0]*100 > 80:
 
-            prediction_dict= {
-                    "response": {
-                        "solutions": {
-                            "re_roomtype_eu_v2": {
-                                
-                                "predictions": [
-                                                    {
-                                                    "confidence": str(top_five[0][0]),
-                                                    "label" : "false"
-                                                    }
-                                                ],
-                                                "top_prediction":{
-                                                    "confidence": str(top_five[0][0]),
-                                                        "label": "false"
+        prediction_dict= {
+                "response": {
+                    "solutions": {
+                        "re_roomtype_eu_v2": {
+                            
+                            "predictions": [
+                                                {
+                                                "confidence": str(top_five[0][0]),
+                                                "label" : "false"
                                                 }
+                                            ],
+                                            "top_prediction":{
+                                                "confidence": str(top_five[0][0]),
+                                                    "label": "false"
                                             }
                                         }
                                     }
                                 }
-            return prediction_dict
-        
-        else:
+                            }
+        return prediction_dict
+    
+    else:
 
             return prediction_dict
 
 
 @app.get("/predict_from_url")
 async def predict_image(image_url: str):
-
-    response = requests.get(image_url)
-    image_bytes = io.BytesIO(response.content)
-    input_image = Image.open(image_bytes).convert("RGB")
-    input_image.save("input_img.jpg")
+    function = predict_image1(image_url)
+    return function
+    # response = requests.get(image_url)
+    # image_bytes = io.BytesIO(response.content)
+    # input_image = Image.open(image_bytes).convert("RGB")
+    # input_image.save("input_img.jpg")
     
-    result = predict.classify(model_nsfw,"input_img.jpg")
-    status_check = result["input_img.jpg"]
+    # result = predict.classify(model_nsfw,"input_img.jpg")
+    # status_check = result["input_img.jpg"]
 
-    final_result = status_check['porn']*100
-    final_result2 = status_check['sexy']*100
+    # final_result = status_check['porn']*100
+    # final_result2 = status_check['sexy']*100
 
-    if  final_result > 30 or final_result2 > 10 :
+    # if  final_result > 30 or final_result2 > 10 :
 
-        prediction_dict= {
-        "response": {
-            "solutions": {
-                "re_roomtype_eu_v2": {
+    #     prediction_dict= {
+    #     "response": {
+    #         "solutions": {
+    #             "re_roomtype_eu_v2": {
                      
-                     "predictions": [
-                                        {
-                                        "confidence": str(final_result),
-                                        "label" : "false"
-                                        }
-                                    ],
-                                    "top_prediction":{
-                                        "confidence": str(final_result),
-                                            "label": "false"
-                                    }
-                                }
-                            }
-                        }
-                    }
+    #                  "predictions": [
+    #                                     {
+    #                                     "confidence": str(final_result),
+    #                                     "label" : "false"
+    #                                     }
+    #                                 ],
+    #                                 "top_prediction":{
+    #                                     "confidence": str(final_result),
+    #                                         "label": "false"
+    #                                 }
+    #                             }
+    #                         }
+    #                     }
+    #                 }
         
-        return prediction_dict
+    #     return prediction_dict
     
-    else:
+    # else:
 
-        function = predict_image1(image_url)
-        return function
+    #     function = predict_image1(image_url)
+    #     return function
 
 def predict_image1(str_url: str):
 
-    response = requests.get(str_url)
+    response = requests.get(str_url) 
+    print("status = ",response.status_code)
     image_bytes = io.BytesIO(response.content)
     img = Image.open(image_bytes)
+    img.show()
     prediction1 = predict_img(img)
     data = json.dumps(prediction1)
     data1 = json.loads(data.replace("\'", '"'))
